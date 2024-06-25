7cc460b6eb3f1d994977ecb1a786335cd598508b
zachbakerdev <zach@zachbaker.dev>
2024-06-25 13:53:38 -0400

    feat: add variant property to internal link

--- src/components/navigation/InternalLink.tsx
+++ src/components/navigation/InternalLink.tsx
@@ -1,14 +1,15 @@
-import { Link } from "@mui/material";
+import { Link, TypographyOwnProps } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 type InternalLinkProps = PropsWithChildren & {
     to: string;
+    variant?: TypographyOwnProps["variant"];
 };
 
-const InternalLink: FC<InternalLinkProps> = ({ to, children }) => {
+const InternalLink: FC<InternalLinkProps> = ({ variant, to, children }) => {
     return (
-        <Link to={to} component={RouterLink}>
+        <Link to={to} component={RouterLink} variant={variant ?? undefined}>
             {children}
         </Link>
     );
