33cf96608b0da52ceb6389db1655c26bee636080
zachbakerdev <zach@zachbaker.dev>
2024-06-27 13:35:33 -0400

    feat: add on click property to internal link

--- src/components/navigation/InternalLink.tsx
+++ src/components/navigation/InternalLink.tsx
@@ -1,15 +1,26 @@
 import { Link, TypographyOwnProps } from "@mui/material";
-import { FC, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 type InternalLinkProps = PropsWithChildren & {
     to: string;
     variant?: TypographyOwnProps["variant"];
+    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
 };
 
-const InternalLink: FC<InternalLinkProps> = ({ variant, to, children }) => {
+const InternalLink: FC<InternalLinkProps> = ({
+    onClick,
+    variant,
+    to,
+    children
+}) => {
     return (
-        <Link to={to} component={RouterLink} variant={variant ?? undefined}>
+        <Link
+            to={to}
+            component={RouterLink}
+            variant={variant ?? undefined}
+            onClick={onClick ?? undefined}
+        >
             {children}
         </Link>
     );
